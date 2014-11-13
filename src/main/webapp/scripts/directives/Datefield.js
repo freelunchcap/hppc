139cea1d258ded1cbd9b48ba81fa5f3bdee9dbe0
beijunyi <beijunyi@gmail.com>
2014-11-13 15:45:04 +0000

    clean impl

--- src/main/webapp/scripts/directives/Datefield.js
+++ src/main/webapp/scripts/directives/Datefield.js
@@ -1,25 +1,40 @@
-app.directive('datefield', function($timeout) {
+app.directive('datefield', function($timeout, $compile) {
   return {
     restrict: 'A',
-    replace: true,
-    templateUrl: 'views/directives/Datefield.html',
     link: function($scope, $elem, $attr) {
+      var element = $elem[0];
 
-      $scope.id = $attr.id;
-      document.querySelector('label[for=' + $scope.id + ']').addEventListener('click', function($event) {
-        $scope.toggle($event);
-      });
+      element.removeAttribute('datefield');
 
-      $scope.toggle = function($event, state) {
+      element.setAttribute('datepicker-popup', 'dd-MM-yyyy');
+      element.setAttribute('is-open', 'opened');
+      element.setAttribute('readonly', '');
+
+      $elem.html($compile(element)($scope));
+
+      function toggle($event) {
         if($event != null) {
           $event.preventDefault();
           $event.stopPropagation();
         }
         $timeout(function() {
-          $scope.opened = state || !$scope.opened
+          $scope.opened = !$scope.opened;
         });
-      };
+      }
 
+      $scope.opened = false;
+      $scope.$watch('opened', function(newValue, oldValue) {
+        if(newValue != oldValue) {
+          if(newValue)
+            element.classList.add('focus');
+          else
+            element.classList.remove('focus');
+        }
+      });
+
+      $scope.id = $attr.id;
+      document.querySelector('label[for=' + $scope.id + ']').addEventListener('click', toggle);
+      element.addEventListener('click', toggle);
     }
   }
 });
