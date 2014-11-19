a408b718a1d611c6ba46232ca95eef8f481f265a
beijunyi <beijunyi@gmail.com>
2014-11-19 17:00:07 +0000

    hppc datefield

---
+++ src/main/webapp/scripts/directives/HppcDatefield.js
@@ -0,0 +1,50 @@
+app.directive('hppcDatefield', function($timeout, $compile) {
+  return {
+    restrict: 'A',
+    link: function($scope, $elem, $attr) {
+      var element = $elem[0];
+      var id = $attr.id;
+
+      element.removeAttribute('hppc-datefield');
+
+      element.setAttribute('datepicker-popup', 'dd-MM-yyyy');
+      element.setAttribute('is-open', '$datefield.' + id);
+      element.setAttribute('readonly', '');
+
+      $compile(element)($scope);
+
+      if($scope.$datefield == null)
+        $scope.$datefield = {
+          $opened: null
+        };
+      $scope.$datefield[id] = false;
+
+      function toggle() {
+        if($scope.$datefield.$opened != null && $scope.$datefield.$opened != id)
+          $scope.$datefield[$scope.$datefield.$opened] = false;
+        $timeout(function() {
+          if($scope.$datefield[id] = !$scope.$datefield[id])
+            $scope.$datefield.$opened = id;
+          else
+            $scope.$datefield.$opened = null;
+        });
+      }
+
+      $scope.$watch('$datefield.' + id, function(newValue, oldValue) {
+        if(newValue != oldValue) {
+          if(newValue)
+            element.classList.add('focus');
+          else
+            element.classList.remove('focus');
+        }
+      });
+
+      document.querySelector('label[for=' + id + ']').addEventListener('click', function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        toggle();
+      });
+      element.addEventListener('click', toggle);
+    }
+  }
+});
