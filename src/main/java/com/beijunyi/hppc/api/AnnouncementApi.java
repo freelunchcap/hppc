f8cbf223315886aa016bb7091ae3f50d2f3c5b57
beijunyi <beijunyi@gmail.com>
2014-11-26 09:15:09 +0800

    minor fixes

--- src/main/java/com/beijunyi/hppc/api/AnnouncementApi.java
+++ src/main/java/com/beijunyi/hppc/api/AnnouncementApi.java
@@ -1,23 +1,21 @@
 package com.beijunyi.hppc.api;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 
-import com.beijunyi.hppc.models.data.forms.BasicInformation;
+import com.beijunyi.hppc.models.data.system.Announcement;
 import com.beijunyi.hppc.services.data.DataService;
 
 @Named
 @Singleton
 @Path("/api/announcement")
-public class AnnouncementApi extends DataApi<BasicInformation> {
+public class AnnouncementApi extends DataApi<Announcement> {
 
   @Inject
-  public AnnouncementApi(@Nonnull DataService<BasicInformation> service) {
+  public AnnouncementApi(@Nonnull DataService<Announcement> service) {
     super(service);
   }
 
--- src/main/webapp/scripts/modules/Announcement/AnnouncementPageController.js
+++ src/main/webapp/scripts/modules/Announcement/AnnouncementPageController.js
@@ -1,3 +1,20 @@
-app.controller('AnnouncementPageController', function() {
+app.controller('AnnouncementPageController', function($scope, $filter, ngTableParams, Announcement) {
+  $scope.tableParams = new ngTableParams({
+    page: 1,
+    count: 25
+  },{
+    counts: [],
+    getData: function($defer, params) {
+      var query = $filter('hppcParamsFilter')(params);
+      Announcement.query(query, function (data, responseHeaders) {
+        params.total(parseInt(responseHeaders('total')));
+        $defer.resolve(data);
+      });
+    }
+  });
+
+  $scope.select = function(record) {
+    $scope.tableParams.$selection = record;
+  };
 
 });
