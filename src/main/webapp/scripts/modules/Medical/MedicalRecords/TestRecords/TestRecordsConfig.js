6579df291403a513104161a01bd83f8a975d0994
beijunyi <beijunyi@gmail.com>
2014-12-10 08:28:57 +0800

    test forms

--- src/main/webapp/scripts/modules/Medical/MedicalRecords/TestRecords/TestRecordsConfig.js
+++ src/main/webapp/scripts/modules/Medical/MedicalRecords/TestRecords/TestRecordsConfig.js
@@ -14,6 +14,14 @@ app.config(function ($stateProvider) {
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/AntigenAntibodyTestRecords/AntigenAntibodyTestRecordsPage.html',
       controller: 'AntigenAntibodyTestRecordsPageController'
     })
+    .state('medical.record.antigen-antibody-test-record', {
+      url: '/antigen-antibody-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/AntigenAntibodyTestRecords/AntigenAntibodyTestRecordForm.html',
+      controller: 'AntigenAntibodyTestRecordPageController'
+    })
     .state('medical.record.biochemistry-test-records', {
       url: '/biochemistry-test-records',
       data: {
@@ -21,6 +29,14 @@ app.config(function ($stateProvider) {
       },
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/BiochemistryTestRecords/BiochemistryTestRecordsPage.html',
       controller: 'BiochemistryTestRecordsPageController'
+    })
+    .state('medical.record.biochemistry-test-record', {
+      url: '/biochemistry-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/BiochemistryTestRecords/BiochemistryTestRecordForm.html',
+      controller: 'BiochemistryTestRecordPageController'
     })
     .state('medical.record.blood-test-records', {
       url: '/blood-test-records',
@@ -30,6 +46,14 @@ app.config(function ($stateProvider) {
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/BloodTestRecords/BloodTestRecordsPage.html',
       controller: 'BloodTestRecordsPageController'
     })
+    .state('medical.record.blood-test-record', {
+      url: '/blood-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/BloodTestRecords/BloodTestRecordForm.html',
+      controller: 'BloodTestRecordPageController'
+    })
     .state('medical.record.feces-test-records', {
       url: '/feces-test-records',
       data: {
@@ -37,6 +61,14 @@ app.config(function ($stateProvider) {
       },
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/FecesTestRecords/FecesTestRecordsPage.html',
       controller: 'FecesTestRecordsPageController'
+    })
+    .state('medical.record.feces-test-record', {
+      url: '/feces-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/FecesTestRecords/FecesTestRecordForm.html',
+      controller: 'FecesTestRecordPageController'
     })
     .state('medical.record.skin-test-records', {
       url: '/skin-test-records',
@@ -46,6 +78,14 @@ app.config(function ($stateProvider) {
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/SkinTestRecords/SkinTestRecordsPage.html',
       controller: 'SkinTestRecordsPageController'
     })
+    .state('medical.record.skin-test-record', {
+      url: '/skin-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/SkinTestRecords/SkinTestRecordForm.html',
+      controller: 'SkinTestRecordPageController'
+    })
     .state('medical.record.urine-test-records', {
       url: '/urine-test-records',
       data: {
@@ -53,6 +93,14 @@ app.config(function ($stateProvider) {
       },
       templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/UrineTestRecords/UrineTestRecordsPage.html',
       controller: 'UrineTestRecordsPageController'
+    })
+    .state('medical.record.urine-test-record', {
+      url: '/urine-test-record/:tid',
+      data: {
+        parents: ['medical.record.test-records']
+      },
+      templateUrl: 'views/modules/Medical/MedicalRecords/TestRecords/UrineTestRecords/UrineTestRecordForm.html',
+      controller: 'UrineTestRecordPageController'
     });
 
 });
